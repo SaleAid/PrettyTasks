e263b06b9f025ba8b5d341a7116ed6e96f9b326b
Vlad Kruglik <krugvs@gmail.com>
2012-04-05 10:57:47 +0300

    DB changes

--- sources/db/0005.ddl.create_table_days.sql
+++ sources/db/0005.ddl.create_table_days.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS `days` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'primary key',
   `user_id` bigint(20) NOT NULL COMMENT 'reference to user',
   `comment` text COMMENT 'comment for day',
-  `rating_id` int(11) DEFAULT '0' COMMENT 'reference to rating',
+  `rating` int(11) DEFAULT '0' COMMENT 'reference to rating',
   `date` date NOT NULL COMMENT 'date of the day',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Table for store days' AUTO_INCREMENT=1 ;
