23e4968a2add0814fe5206dcb0bd0d4df90876f1
Alexandr Frankovskiy <afrankovskiy@gmail.com>
2012-05-25 23:12:52 +0300

    redirect to tasks if user login

--- sources/webapplication/app/Controller/PagesController.php
+++ sources/webapplication/app/Controller/PagesController.php
@@ -9,6 +9,12 @@ class PagesController extends AppController {
     public function beforeFilter(){
         parent::beforeFilter();
         $this->Auth->allow('*');
+        if( $this->Auth->loggedIn() ) {
+            $this->redirect(array(
+                'controller' => 'tasks',
+                'action' => 'index'
+        ));
+        }
 
     }
     
