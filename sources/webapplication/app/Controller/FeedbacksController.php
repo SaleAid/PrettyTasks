c909bd82e5d85e044317850f3080a44068af40de
Alexandr Frankovskiy <afrankovskiy@gmail.com>
2012-06-15 06:46:13 +0300

    updated feedback

--- sources/webapplication/app/Controller/FeedbacksController.php
+++ sources/webapplication/app/Controller/FeedbacksController.php
@@ -6,50 +6,49 @@ class FeedbacksController extends AppController {
         'User'
     );
 
-    public function _getAllowedActions() {
-        return array(
-            'add_from_application', 
-            'add'
-        );
-    }
-
-    public function beforeFilter() {
-        $this->Auth->allow($this->_getAllowedActions());
-        parent::beforeFilter();
-    }
-
+    
     public function add() {
-        if (! empty($this->request->data)) {
-            $feed['Feedback']['lang'] = Configure::read('Config.language');
-            $feed['Feedback']['category'] = $this->data['Feedback']['category'];
-            $feed['Feedback']['status'] = 1;
-            $feed['Feedback']['subject'] = $this->request->data['Feedback']['subject'];
-            $feed['Feedback']['message'] = $this->request->data['Feedback']['message'];
-            $feed['Feedback']['processed'] = 0;
-            $feed['Feedback']['user_id'] = $this->Auth->user('id');
-            if ($this->Feedback->save($feed)) {
-                $this->Session->setFlash(__('Ваше сообщение было сохранено', true), 'alert', array(
-                            'class' => 'alert-success'
-                        ));
-                $this->redirect(array(
-                    'action' => 'add'
-                ));
+        if ($this->request->is('post')) {
+            $expectedData = array(
+                'category',
+                'subject',
+                'message'
+            );
+            if (!$this->_isSetRequestData($expectedData, $this->modelClass)) {
+                $this->Session->setFlash(__('Ошибка при передачи данных'), 'alert', array(
+                        'class' => 'alert-error'
+                    ));
             } else {
-                $messages ='';
-                foreach($this->Feedback->validationErrors as $items){
-                    if(is_array($items)){
-                        foreach($items as $item){
-                            if(!empty($item)){
-                                $messages = $messages.$item.'<br>';
+                $feed['Feedback']['lang'] = Configure::read('Config.language');
+                $feed['Feedback']['category'] = $this->data['Feedback']['category'];
+                $feed['Feedback']['status'] = 1;
+                $feed['Feedback']['subject'] = $this->request->data['Feedback']['subject'];
+                $feed['Feedback']['message'] = $this->request->data['Feedback']['message'];
+                $feed['Feedback']['processed'] = 0;
+                $feed['Feedback']['user_id'] = $this->Auth->user('id');
+                if ($this->Feedback->save($feed)) {
+                    $this->Session->setFlash(__('Ваше сообщение было сохранено', true), 'alert', array(
+                                'class' => 'alert-success'
+                            ));
+                    $this->redirect(array(
+                        'action' => 'add'
+                    ));
+                } else {
+                    $messages ='';
+                    foreach($this->Feedback->validationErrors as $items){
+                        if(is_array($items)){
+                            foreach($items as $item){
+                                if(!empty($item)){
+                                    $messages = $messages.$item.'<br>';
+                                }
                             }
                         }
                     }
+                $this->Session->setFlash(__($messages.'Ваше сообщение не удалось сохранить. Попробуйте снова.', true), 'alert', array(
+                                        'class' => 'alert-error'
+                ));    
                 }
-            $this->Session->setFlash(__($messages.'Ваше сообщение не удалось сохранить. Попробуйте снова.', true), 'alert', array(
-                                    'class' => 'alert-error'
-            ));    
             }
         }
     }
-
 }
