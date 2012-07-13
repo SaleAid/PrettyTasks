feb3398ddd1d0765ba02cb38976a7104fd4d17a7
Alexandr Frankovskiy <afrankovskiy@gmail.com>
2012-07-13 19:54:43 +0300

    modified loginza helper

--- sources/webapplication/app/View/Helper/LoginzaHelper.php
+++ sources/webapplication/app/View/Helper/LoginzaHelper.php
@@ -5,6 +5,9 @@ class LoginzaHelper extends AppHelper {
     public function getCssUrl() {
         return 'loginza/widget_style.css';
     }
+    public function getJs(){
+        return 'http://loginza.ru/js/widget.js';
+    }
 
     public function ico($provider) {
         if (! $provider) {
@@ -18,5 +21,33 @@ class LoginzaHelper extends AppHelper {
             return '';
         }
         return "<span class=\"providers_sprite {$provider}\">&nbsp;</span>";
+    }
+    
+    public function buttonWidget($token_url){
+       $str = '<a href="http://loginza.ru/api/widget?token_url='.$token_url;
+       $str .=' &providers_set=vkontakte,facebook,twitter,google';
+       $str .='&lang='.Configure::read('Config.langURL');
+       $str .='" class="loginza">';
+       $str .= '<img src="http://loginza.ru/img/sign_in_button_gray.gif" alt="Войти через loginza"/>';
+       $str .= '</a>';
+       return $str;
+    }
+    
+    public function iframeWidget($token_url){
+        $str = ' <iframe src="http://loginza.ru/api/widget?overlay=loginza&token_url='.$token_url;
+        $str .='&lang='.Configure::read('Config.langURL');
+        $str .=' &providers_set=vkontakte,facebook,twitter,google"'; 
+        $str .=' style="width:330px;height:244px;" scrolling="no" frameborder="no">';
+        $str .=' </iframe>';
+        return $str;
+    }
+    public function iconsWidget($token_url, $provider){
+        $str = '<a href="https://loginza.ru/api/widget?provider='.$provider.'&token_url='.$token_url;
+        $str .='&providers_set=vkontakte,facebook,twitter,google'; 
+        $str .='&lang='.Configure::read('Config.langURL');
+        $str .='" class="loginza">';
+        $str .='<img src="http://loginza.ru/img/providers/'.$provider.'.png" alt="'.$provider.'" title="'.$provider.'">';
+        $str .='</a>';
+        return $str;
     }
 }
