28052f3e9ab7cb452c451a62a510052816b40158
Alexandr Frankovskiy <afrankovskiy@gmail.com>
2012-11-20 21:09:54 +0200

    added OAuth plugin

---
+++ sources/webapplication/app/Plugin/OAuth/Controller/OAuthAppController.php
@@ -0,0 +1,18 @@
+<?php
+
+App::import('Vendor', 'oauth2-php/lib/OAuth2');
+App::import('Vendor', 'oauth2-php/lib/IOAuth2Storage');
+App::import('Vendor', 'oauth2-php/lib/IOAuth2GrantCode');
+App::import('Vendor', 'oauth2-php/lib/IOAuth2RefreshTokens');
+
+/**
+ * Description of OAuthAppController
+ *
+ * @author Thom
+ */
+class OAuthAppController extends AppController {
+    //public function beforeFilter() {}
+    public function _setLanguage() {}
+}
+
+?>
