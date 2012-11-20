44c75f1982422af8757b214bfcb369ec34fab253
Alexandr Frankovskiy <afrankovskiy@gmail.com>
2012-11-20 21:11:43 +0200

    task object for api

---
+++ sources/webapplication/app/Lib/TaskObj.php
@@ -0,0 +1,18 @@
+<?php 
+
+class TaskObj {
+    
+    public $id = null;
+    public $title = null;
+    public $date= null;
+    public $time = null;
+    public $timeend = null;
+    public $priority = null;
+    public $order = null;
+    public $future = null;
+    public $deleted = null;
+    public $done = null;
+    public $datedone = null;
+    public $comment = null;
+    
+} 
