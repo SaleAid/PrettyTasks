eb4221ba99f3aafe692e9e52fe451f774fc57718
Alexandr Frankovskiy <afrankovskiy@gmail.com>
2013-04-18 19:35:27 +0300

    added behavior Ordered (yet empty)

--- sources/webapplication/app/Model/Behavior/OrderedBehavior.php
+++ sources/webapplication/app/Model/Behavior/OrderedBehavior.php
@@ -11,7 +11,7 @@ class OrderedBehavior extends ModelBehavior {
  */
 	public $settings = array();
     
-    protected $_createOfFirst = false;
+    protected $_startOrder = 1;
     
 /**
  * Default settings
@@ -44,9 +44,11 @@ class OrderedBehavior extends ModelBehavior {
 		$this->settings[$model->alias] = array_merge($this->settings[$model->alias], $config);
 		$this->settings[$model->alias]['withModel'] = $this->settings[$model->alias]['orderedClass'];
 		
+        $model->data[$model->alias]['oList'] = null;
+		$model->data[$model->alias]['createFirst'] = false;
+        
         extract($this->settings[$model->alias]);
-
-		$model->bindModel(array('hasOne' => array(
+        $model->bindModel(array('hasOne' => array(
 			$orderedAlias => array(
 				'className' => $orderedClass,
 				'foreignKey' => $foreignKey,
@@ -54,7 +56,7 @@ class OrderedBehavior extends ModelBehavior {
 				'conditions' => array(
 					'Ordered.model' => $model->name),
 				'fields' => '',
-				'dependent' => true,
+                'dependent' => true,
 				'with' => $withModel))), $resetBinding);
 
 	}
@@ -75,26 +77,33 @@ class OrderedBehavior extends ModelBehavior {
         
         $orderedAlias = $this->settings[$Model->alias]['orderedAlias']; 
         $orderedModel = $Model->{$orderedAlias};
-        $oList = $Model->data[$Model->alias]['date'];
-        $order = 1; 
-        if( !$this->_createOfFirst ){
+        
+        if( !$Model->data[$Model->alias]['oList'] ){
+            return false; 
+        }
+        $oList = $Model->data[$Model->alias]['oList'];
+        $order = $this->_startOrder; 
+        if( !$Model->data[$Model->alias]['createFirst'] ){
             $order = $this->_newOrder($Model, $oList);
         }
         $data = array(
             $this->settings[$Model->alias]['foreignKey'] => $Model->data[$Model->alias][$Model->primaryKey],
             'list' => $oList,
             'model' => $Model->alias,
+            'user_id' => $Model->data[$Model->alias]['user_id'], 
             $this->settings[$Model->alias]['field'] => $order
         );
-        
-        
         $orderedModel->create($data);
         $orderedModel->save();
     }
     
     private function _highest(Model $Model, $oList) {
 		$options = array(
-                'conditions' => array('list' => $oList, 'model' => $Model->alias),
+                'conditions' => array(
+                                      'list' => $oList, 
+                                      'model' => $Model->alias, 
+                                      'user_id' => $Model->data[$Model->alias]['user_id']
+                ),
 				'order' => $this->settings[$Model->alias]['field'] . ' DESC', 
 				'fields' => array($this->settings[$Model->alias]['field']), 
 				'recursive' => -1);
@@ -107,7 +116,7 @@ class OrderedBehavior extends ModelBehavior {
 	private function _newOrder($Model, $oList) {
 		$highest = $this->_highest($Model, $oList);
         if(empty($highest)){
-            return 1;
+            return $this->_startOrder;
         }
         return $highest[$this->settings[$Model->alias]['orderedAlias']][$this->settings[$Model->alias]['field']] + 1;		
 	}	
