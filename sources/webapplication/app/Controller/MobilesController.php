4406d1c23c4b8ceec032df00ff4c3e561d64d89b
Vlad Kruglik <krugvs@gmail.com>
2013-08-07 17:21:15 +0300

    Lists for mobile version

---
+++ sources/webapplication/app/Controller/MobilesController.php
@@ -0,0 +1,14 @@
+<?php
+App::uses('AppController', 'Controller');
+class MobilesController extends AppController {
+    
+    public $name = 'Mobiles';
+    public $uses = null;
+    
+    
+    public function index(){
+        $this->layout = 'mobile';
+        
+    }
+    
+}
