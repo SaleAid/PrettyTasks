dfef36b62094c6c1ff9371e5051ee321c1e518bd
Vlad Kruglik <krugvs@gmail.com>
2014-02-01 16:11:30 +0200

    New email accounts

--- sources/webapplication/app/Config/email.php
+++ sources/webapplication/app/Config/email.php
@@ -1,65 +1,87 @@
 <?php
 
 class EmailConfig {
+    /**
+     * 
+     * @var array
+     */
+    public $default = array(
+            'transport' => 'Smtp',
+            'host' => 'smtp.mandrillapp.com',
+            'port' => 587,
+            'username' => 'prettytasks@gmail.com',
+            'password' => 'PG8cWfu51HWrQeLn6UJZ5Q',
+            'charset' => 'utf-8',
+            'headerCharset' => 'utf-8',
+            'from' => array(
+                    'info@prettytasks.com' => 'PrettyTasks Online Organizer'
+            ),
+            'replyTo' => array(
+                    'info@prettytasks.com' => 'PrettyTasks Online Organizer'
+            )
+    );
+    
+    /**
+     * This config is used for Account email
+     *
+     * @var array
+     */
+    public $account = array(
+            'transport' => 'Smtp',
+            'host' => 'smtp.mandrillapp.com',
+            'port' => 587,
+            'username' => 'prettytasks@gmail.com',
+            'password' => 'PG8cWfu51HWrQeLn6UJZ5Q',
+            'charset' => 'utf-8',
+            'headerCharset' => 'utf-8',
+            'from' => array(
+                    'info@prettytasks.com' => 'PrettyTasks Online Organizer'
+            ),
+            'replyTo' => array(
+                    'info@prettytasks.com' => 'PrettyTasks Online Organizer'
+            )
+    );
+    
+    /**
+     * This config is used for Feedback email
+     *
+     * @var array
+     */
+    public $feedback = array(
+    		'transport' => 'Smtp',
+    		'host' => 'smtp.mandrillapp.com',
+    		'port' => 587,
+    		'username' => 'prettytasks@gmail.com',
+    		'password' => 'PG8cWfu51HWrQeLn6UJZ5Q',
+    		'charset' => 'utf-8',
+    		'headerCharset' => 'utf-8',
+    		'from' => array(
+    				'info@prettytasks.com' => 'PrettyTasks Online Organizer'
+    		),
+    		'replyTo' => array(
+    				'info@prettytasks.com' => 'PrettyTasks Online Organizer'
+    		)
+    );
 
-	public $default = array(
-	    'transport' => 'Smtp',
-	    'host' => 'smtp.mandrillapp.com',
-		'port' => 587,
-		'username' => 'prettytasks@gmail.com',
-		'password' => 'PG8cWfu51HWrQeLn6UJZ5Q',
-	    'charset' => 'utf-8',
-	    'headerCharset' => 'utf-8',
-        'from'      => array('info@prettytasks.com' => 'PrettyTasks Online Organizer'),
-        'replyTo'   => array('info@prettytasks.com' => 'PrettyTasks Online Organizer')
-	        
-	        
-	);
-
-	public $smtp = array(
-		'transport' => 'Smtp',
-		'from' => array('site@localhost' => 'My Site'),
-		'host' => 'localhost',
-		'port' => 25,
-		'timeout' => 30,
-		'username' => 'user',
-		'password' => 'secret',
-		'client' => null,
-		'log' => false
-		//'charset' => 'utf-8',
-		//'headerCharset' => 'utf-8',
-	);
-
-	public $fast = array(
-		'from' => 'you@localhost',
-		'sender' => null,
-		'to' => null,
-		'cc' => null,
-		'bcc' => null,
-		'replyTo' => null,
-		'readReceipt' => null,
-		'returnPath' => null,
-		'messageId' => true,
-		'subject' => null,
-		'message' => null,
-		'headers' => null,
-		'viewRender' => null,
-		'template' => false,
-		'layout' => false,
-		'viewVars' => null,
-		'attachments' => null,
-		'emailFormat' => null,
-		'transport' => 'Smtp',
-		'host' => 'localhost',
-		'port' => 25,
-		'timeout' => 30,
-		'username' => 'user',
-		'password' => 'secret',
-		'client' => null,
-		'log' => true,
-		//'charset' => 'utf-8',
-		//'headerCharset' => 'utf-8',
-	);
-
+    /**
+     * This config is used for invitation email
+     *
+     * @var array
+     */
+    public $invitation = array(
+    		'transport' => 'Smtp',
+    		'host' => 'smtp.mandrillapp.com',
+    		'port' => 587,
+    		'username' => 'prettytasks@gmail.com',
+    		'password' => 'PG8cWfu51HWrQeLn6UJZ5Q',
+    		'charset' => 'utf-8',
+    		'headerCharset' => 'utf-8',
+    		'from' => array(
+    				'info@prettytasks.com' => 'PrettyTasks Online Organizer'
+    		),
+    		'replyTo' => array(
+    				'info@prettytasks.com' => 'PrettyTasks Online Organizer'
+    		)
+    );
 
 }
